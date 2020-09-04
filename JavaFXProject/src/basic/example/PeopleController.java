08d601df3a6b93a1d2cce9f90a0099ff23e51efc
limlim1234 <ehrgkrpskf@gmail.com>
2020-09-04 14:42:12 +0900

    9.4

--- JavaFXProject/src/basic/example/PeopleController.java
+++ JavaFXProject/src/basic/example/PeopleController.java
@@ -27,28 +27,36 @@ public class PeopleController implements Initializable {
 		ObservableList<Data> list = FXCollections.observableArrayList();
 		XYChart.Series<String, Integer> s1 = new XYChart.Series<>();
 		s1.setData(getSeries1());
-		s1.setName("남자");
+		s1.setName("국어");
 
 		XYChart.Series<String, Integer> s2 = new XYChart.Series<>();
-		s2.setData(getSeries2());
-		s2.setName("여자");
+		s2.setData(getSeries1());
+		s2.setName("수학");
+		
+		XYChart.Series<String, Integer> s3 = new XYChart.Series<>();
+		s3.setData(getSeries1());
+		s3.setName("영어");
 
 		barChart.getData().add(s1);
 		barChart.getData().add(s2);
+		barChart.getData().add(s3);
 		
 	}
 	
 	
-	public ObservableList<XYChart.Data<String, Integer>> getSeries1() {
+	public ObservableList<People> getPeople() {
 		Connection conn = ConnectionDB.getDB();
 		String sql = "select * from student";
-		ObservableList<XYChart.Data<String, Integer>> list = FXCollections.observableArrayList();
+		ObservableList<People> list = FXCollections.observableArrayList();
 		try {
 
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
-				list.add(new XYChart.Data<>(rs.getInt("korean"), rs.getInt("math"), rs.getInt("english");
+				People people = new People(rs.getString("id"), 
+						rs.getString("name"),rs.getInt("korean"), 
+						rs.getInt("math"), rs.getInt("english");
+				list.add(people);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
